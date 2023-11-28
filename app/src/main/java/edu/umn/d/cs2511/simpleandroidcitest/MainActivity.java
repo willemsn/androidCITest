8a434e347a5a4d60b3d24c6b2668285c9a752436
Pete Willemsen <willemsn@d.umn.edu>
2023-11-28 10:18:42 -0600

    Initial commit

---
+++ app/src/main/java/edu/umn/d/cs2511/simpleandroidcitest/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.umn.d.cs2511.simpleandroidcitest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
